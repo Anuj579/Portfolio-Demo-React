ee7d1e2264bd0ffe8e5b5760e19342a1bd56dd9f
Anuj Chaudhary <alwaysmile9813@gmail.com>
2024-05-23 22:33:12 +0530

    updates

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -8,25 +8,39 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div
-      style={{
-        position: "fixed",
-        display: "flex",
-        justifyContent: "center",
-        gap: "2rem",
-        background: "rgba(0,0,0)",
-        color:'white',
-        padding: "1rem",
-        top: 0,
-        width: "100%",
-        zIndex: 10,
-      }}
-    >
-      <a href="#home">Home</a>
-      <a href="#about">About</a>
-      <a href="#portfolio">Portfolio</a>
-      <a href="#footer">Contact</a>
-    </div>
+    <>
+      <div className="header-div"
+        style={{
+          position: "fixed",
+          background: "rgba(0,0,0)",  
+          color: 'white',
+          top: 0,
+          width: "100%",
+          zIndex: 10,
+        }}
+      >
+        <div className="nav-lg">
+          <a href="#home">Home</a>
+          <a href="#about">About</a>
+          {/* <a href="#about">Certification</a> */}
+          <a href="#portfolio">Portfolio</a>
+          <a href="#footer">Contact</a>
+        </div>
+
+        {/* <div className="nav-sm">
+          <div className="navToggler">
+            <a><i className="fa-solid fa-bars fa-xl"></i></a>
+          </div>
+          <div style={{ display: "flex", flexDirection: "column" }}>
+            <a href="#home">Home</a>
+            <a href="#about">About</a>
+            <a href="#about">Certification</a>
+            <a href="#portfolio">Portfolio</a>
+            <a href="#footer">Contact</a>
+          </div>
+        </div> */}
+      </div>
+    </>
   );
 };
 
